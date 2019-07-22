44b94e112b1bfe7cd4441d911870e7ffb7b8fe99
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2019-07-22 15:31:08 +0200

    added first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+
+var express = require('express')
+var app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`)
+})
