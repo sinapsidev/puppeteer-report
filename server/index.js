87717f869e5b7c7d8f7e5d825df32a9637a7a20a
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2019-07-22 15:37:35 +0200

    pdf example

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 
-var express = require('express')
-var app = express()
+const express = require('express')
+const puppeteer = require('puppeteer')
+
+const app = express()
 
 const PORT = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.post('/print', async (req, res) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.medium.com')
+  const buffer = await page.pdf({ format: 'A4' })
+  await browser.close()
+  res.type('application/pdf')
+  res.send(buffer)
 })
 
 app.listen(PORT, function () {
