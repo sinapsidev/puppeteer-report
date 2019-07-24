fbe5b8d6fb73643f28d798af0f741966729dba24
Antonio Dell'Ava <antonio.dellava@gmail.com>
2019-07-24 08:56:48 +0200

    add page numbers

--- server/index.js
+++ server/index.js
@@ -17,8 +17,22 @@ app.post('/print', async (req, res) => {
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(req.body)
-    const buffer = await page.pdf({ format: 'A4' })
+    await page.goto(req.body, {waitUntil: 'domcontentloaded'});
+
+    const FOOTER_TEMPLATE = `
+    <div id="header-template" style="font-size:10px !important; color:#808080; padding-left:10px">Pagina <span class="pageNumber"></span> di <span class="totalPages"></span></div>
+    `;
+
+    const buffer = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      displayHeaderFooter: true,
+      footerTemplate: FOOTER_TEMPLATE,
+      margin: {
+        top: '0mm',
+        bottom: '10mm'
+      }
+    });
     await browser.close()
     res.type('application/pdf')
     res.send(buffer)
