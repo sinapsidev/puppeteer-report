eceb94745e9046346129d48b0a1c9873f388302c
Antonio Dell Ava <antonio.dellava@gmail.com>
2019-07-26 21:24:13 +0200

    calculate header and footer height dinamically

--- server/index.js
+++ server/index.js
@@ -13,15 +13,41 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+
+// bau bau bau
+const equalizeFont = (HTML, fontSize) => {
+  const correctFontSize = fontSize*0.7;
+  return `<div style="font-size: ${correctFontSize}px; width: 100%;">${HTML}</div>`;
+}
+
+const getFontSizeInIntByFontSizeInPX = computedHeight => {
+  const heightWithoutPX = computedHeight.split("px").join("");
+  const heightFloat = parseInt(heightWithoutPX);
+  return heightFloat;
+};
+
+
 app.post('/print', async (req, res) => {
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(req.body, {waitUntil: 'domcontentloaded'});
+    await page.goto(req.body, {"waitUntil" : "networkidle0"});
 
-    
-    let HEADER_TEMPLATE = await page.$eval('#header', e => e.innerHTML);
-    let FOOTER_TEMPLATE = await page.$eval('#footer', e => e.innerHTML);
+    const HEADER_TEMPLATE = await page.$eval('#header', e => e.innerHTML);
+    const FOOTER_TEMPLATE = await page.$eval('#footer', e => e.innerHTML);
+
+    const HEADER_FONT_SIZE_PX = await page.$eval('#header', e => getComputedStyle(e).fontSize);
+    const HEADER_FONT_SIZE_INT = getFontSizeInIntByFontSizeInPX(HEADER_FONT_SIZE_PX);
+    const FOOTER_FONT_SIZE_PX = await page.$eval('#footer', e => getComputedStyle(e).fontSize);
+    const FOOTER_FONT_SIZE_INT = getFontSizeInIntByFontSizeInPX(FOOTER_FONT_SIZE_PX);
+
+    const SBECCO = 25;
+
+    const HEADER_H = await page.$eval('#header', e => e.getBoundingClientRect().height)+SBECCO;
+    const FOOTER_H = await page.$eval('#footer', e => e.getBoundingClientRect().height)+SBECCO;
+
+    console.log("HEADER_H", HEADER_H)
+    console.log("FOOTER_H", FOOTER_H)
 
   
     await page.evaluate(() => {
@@ -32,11 +58,11 @@ app.post('/print', async (req, res) => {
     const buffer = await page.pdf({
       format: 'A4',
       displayHeaderFooter: true,
-      headerTemplate: HEADER_TEMPLATE,
-      footerTemplate: FOOTER_TEMPLATE,
+      headerTemplate: equalizeFont(HEADER_TEMPLATE, HEADER_FONT_SIZE_INT),
+      footerTemplate: equalizeFont(FOOTER_TEMPLATE, FOOTER_FONT_SIZE_INT),
       margin: {
-        top: '30mm',
-        bottom: '30mm'
+        top: HEADER_H+'px',
+        bottom: FOOTER_H+'px'
       }
     });
     await browser.close()
