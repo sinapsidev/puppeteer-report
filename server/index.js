61d4ffb63240038ccab48f7bb32a14778dc54cda
Antonio Dell'Ava <antonio.dellava@gmail.com>
2019-09-03 09:48:05 +0200

    migliora il calcolo degli ingombri e la gestione tipografica di footer e header

--- server/index.js
+++ server/index.js
@@ -13,11 +13,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+const CSS_RESET = "<style>html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{border:0;font-size:100%;font:inherit;vertical-align:baseline;margin:0;padding:0}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:none}table{border-collapse:collapse;border-spacing:0}*{font-family:Arial,Helvetica,sans-serif !important;}</style>"
 
 // bau bau bau
 const equalizeFont = (HTML, fontSize) => {
-  const correctFontSize = fontSize*0.7;
-  return `<div style="font-size: ${correctFontSize}px; width: 100%;">${HTML}</div>`;
+  const correctFontSize = fontSize*0.54;
+  return `${CSS_RESET}<div style="font-size: ${correctFontSize}px; width: 100%;">${HTML}</div>`;
 }
 
 const getFontSizeInIntByFontSizeInPX = computedHeight => {
