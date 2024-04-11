b36bac612b297424eea2a7ab5c997e416fdaaaa8
mattyk0207 <mattia.lodi@gmail.com>
2024-04-11 21:41:17 +0200

    aggiunte chiamate asincrone e coda dei processi

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ const Fastify = require('fastify');
 const fetch = require('node-fetch');
 const browserFactory = require('./lib/browser')(logger);
 
+// async functions
+const jobs = require('./lib/async-queue/jobs.js');
+const startDashboard = require('./lib/async-queue/uiDashboard.js')(jobs.getQueue());
+await jobs.startWorker()
+
 const PORT = process.env.PORT || 5000;
 const URL = process.env.URL || 'http://localhost:8080';
 const DOMAIN = process.env.DOMAIN || 'http://localhost:8080';
@@ -34,6 +39,7 @@ printerFactory({
     return { hello: 'Hello from Puppeteer Report' };
   });
 
+  /* sync call */
   app.post('/print/:tenantId/:templateId/:recordId', async (req, res) => {
     try {
       const authorization = req.headers.authorization;
@@ -81,8 +87,88 @@ printerFactory({
     }
   });
 
+  /* async calls */
+  app.post('/jobs/:tenantId/:templateId/:recordId', async (req, res) => {
+    try {
+      const authorization = req.headers.authorization;
+      const timeZone = req.headers['time-zone'];
+
+      const {
+        tenantId,
+        templateId,
+        recordId
+      } = req.params;
+
+      const profile = await auth.getProfile({
+        timeZone,
+        token: authorization,
+        tenantId
+      });
+
+      if (!profile) {
+        logger.error('Unauthorized');
+        res.code(401).send({});
+        return;
+      }
+
+      const token = authorization.split(' ')[1];
+
+      const { status, jobId } = await jobs.startJob({
+        args: {
+          body: req.body,
+        tenantId,
+        templateId,
+        recordId,
+        token,
+        domain: DOMAIN,
+        timeZone
+        },
+        printer: printer,
+      }, 10);
+
+      res.send({
+        status,
+        jobId,
+      });
+    } catch (e) {
+      console.error(e.message);
+      res.code(500).send(e.message);
+    }
+  });
+
+  app.get('/jobs/status/:jobId', async (req, res) => {
+    try {
+
+      const { jobId } = req.params;
+
+      const status = jobs.getJobStaus(jobId);
+
+      res.send({ status })
+
+    } catch (e) {
+      console.error(e.message);
+      res.code(500).send(e.message);
+    }
+  });
+
+  app.get('/jobs/:jobId', async (req, res) => {
+    try {
+
+      const { jobId } = req.params;
+
+      const result = jobs.getJobResult(jobId);
+
+      res.send({ result })
+
+    } catch (e) {
+      console.error(e.message);
+      res.code(500).send(e.message);
+    }
+  });
+
+
   try {
-    await app.listen({ port: PORT, host: '0.0.0.0' });
+    await app.listen({ port: PORT });
     console.log('Puppeteer Report ready with Fastify on port ', PORT);
   } catch (e) {
     app.log.error(e);
---
+++ server/lib/async-queue/jobs.js
@@ -0,0 +1,80 @@
+const Queue = require('bull');
+const MAX_CONCURRENT_PROCESS = 2
+const REDIS_HOST = '192.168.0.40'
+// const REDIS_HOST = '127.0.0.1'
+
+
+// const printingJobsQueue = new Queue('printing-jobs-queue', 'redis://' + REDIS_HOST); //, {limiter: {max: 1000,duration: 45000}}
+
+
+module.exports = (function () {
+    function SingletonClass() {
+        return new Queue('printing-jobs-queue', {
+            redis: { host: REDIS_HOST, port: '6379', },
+            limiter: { max: 10, duration: 10000 },      // venfgono eseguiti al massimo max job ogni duration
+        });
+    }
+    var printingJobsQueue;
+
+    return {
+        getQueue: function () {
+            if (printingJobsQueue == null) {
+                printingJobsQueue = new SingletonClass();
+                printingJobsQueue.constructor = null;
+            }
+            return printingJobsQueue;
+        },
+        startJob: async function (jobData, priority=10) {
+            if(!printingJobsQueue) this.getQueue();
+            const job = await printingJobsQueue.add(jobData, { priority });
+            return {
+                jobId: job.id,
+                status: await job.getState(),
+            }
+        },
+        startWorker: async function () {
+            if(!printingJobsQueue) this.getQueue();
+            printingJobsQueue.process(MAX_CONCURRENT_PROCESS, async (job, done) => {
+                job.progress(0);
+                
+                const result = await job.data.printer.print(job.data.args);
+
+                job.progress(100);
+                done(null, result);
+            })
+            console.log(`Worker job started`);
+        }, 
+        getJob: async function (id) {
+            if(!printingJobsQueue) 
+                throw new Error("No process queue instanciated");
+            const job = await printingJobsQueue.getJob(id);
+            if(job)
+                return job;
+            else
+                throw new Error("No job with id "+id);
+        },    
+        getJobStatus: async function (id) {
+            if(!printingJobsQueue) 
+                throw new Error("No process queue instanciated");
+            const job = await printingJobsQueue.getJob(id)
+            if (job)
+                return await job.getState();
+            else
+                throw new Error("No job with id "+id);
+        },
+        getJobResult: async function (id) {
+            if(!printingJobsQueue) 
+                throw new Error("No process queue instanciated");
+            const job = await printingJobsQueue.getJob(id)
+            if (job) {
+                const state = await job.getState();
+                if(state === "completed") 
+                    return job.returnvalue
+                else 
+                    throw new Error("Job with id "+id+" is not completed");
+            }
+            else
+                throw new Error("No job with id "+id);
+        },
+    };
+})();
