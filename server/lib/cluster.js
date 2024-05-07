c72f45666692fc77eefabfc5e5b67ab71b2ffe04
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2024-05-07 15:54:47 +0200

    aggiunta concurrency da env

--- server/lib/cluster.js
+++ server/lib/cluster.js
@@ -3,7 +3,7 @@ const { Cluster } = require('puppeteer-cluster');
 const create = async () => {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 2,
+    maxConcurrency: process.env.MAX_CONCURRENCY || 8,
     retryDelay: 5000,
     retryLimit: 3,
     timeout: 90000,
