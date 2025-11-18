5204499693e7fd6fbfaa9ce43d4691191bda8581
valentinamarrazzo <valemarr99@gmail.com>
2025-11-18 17:02:48 +0100

    style(app): formattazione file

--- server/public/app.js
+++ server/public/app.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  
+
   window.angular.module('reportApp', [
     // MODULES
     'reportApp.auth',
@@ -68,7 +68,8 @@
           const objToAssign = {};
 
           resScheda.forEach((record, index) => {
-            // TO-DO: MODIFICARE QUESTA RIGA PER LAVORARE SULLA CREAZIONE DI REPORT CONTENENTI DATI DI VISTE MULTIPLE
+            // TO-DO: MODIFICARE QUESTA RIGA PER LAVORARE SULLA CREAZIONE DI REPORT CONTENENTI DATI DI UNA STESSA SCHEDA 
+            // MA MULTIPLI ID RECORDS
             if (index > 0) return;
 
             Object.assign(objToAssign, reportHelpers.mapSchedaToReportData(infoScheda, record.data));
@@ -78,58 +79,59 @@
         };
 
         try {
-            const url = new URL(window.location.href);
-            const searchParams = url.searchParams;
-            const idTemplate = parseInt(searchParams.get('idTemplate'), 10);
-            const intIdRecord = handleIdRecordsParams.getIntIdRecord(searchParams);
-            const arrayIdRecords = handleIdRecordsParams.getArrayIdRecords(searchParams);
-            const tenantId = parseInt(searchParams.get('tenantId'), 10);
+          const url = new URL(window.location.href);
+          const searchParams = url.searchParams;
+          const idTemplate = parseInt(searchParams.get('idTemplate'), 10);
+          const intIdRecord = handleIdRecordsParams.getIntIdRecord(searchParams);
+          const arrayIdRecords = handleIdRecordsParams.getArrayIdRecords(searchParams);
+          const tenantId = parseInt(searchParams.get('tenantId'), 10);
 
-            currentUser.changeTenant(tenantId);
+          currentUser.changeTenant(tenantId);
 
-            $scope.stampaDataOra = () => window.moment().toDate();
+          $scope.stampaDataOra = () => window.moment().toDate();
 
-            let idScheda;
-            let infoScheda;
-            let idViste;
-            let template;
-            let visteCorrelate;
-            let infoBase;
+          let idScheda;
+          let infoScheda;
+          let idViste;
+          let template;
+          let visteCorrelate;
+          let infoBase;
 
-            $scope.loading = true;
-            $scope.error = false;
+          $scope.loading = true;
+          $scope.error = false;
 
-            Promise.all([
-              reportService.getTemplate(idTemplate),
-              reportService.getVisteTemplate(idTemplate),
-              reportService.getInfoBase()
-            ]).then(function (res) {
-              template = res[0];
+          Promise.all([
+            reportService.getTemplate(idTemplate),
+            reportService.getVisteTemplate(idTemplate),
+            reportService.getInfoBase()
+          ]).then(function (res) {
+            template = res[0];
 
-              visteCorrelate = res[1].viste || [];
-              infoBase = res[2];
-              Object.assign($scope, {
-                infoBase
-              });
+            visteCorrelate = res[1].viste || [];
+            infoBase = res[2];
+            Object.assign($scope, {
+              infoBase
+            });
 
-              Object.assign($scope.infoBase, {
-                idRecord: intIdRecord,
-                idRecords: arrayIdRecords,
-              });
+            Object.assign($scope.infoBase, {
+              idRecord: intIdRecord,
+              idRecords: arrayIdRecords,
+            });
 
-              idScheda = reportHelpers.getIdScheda(template);
-              idViste = reportHelpers.getIdViste(template);
+            idScheda = reportHelpers.getIdScheda(template);
+            idViste = reportHelpers.getIdViste(template);
 
-              idViste = [...new Set(idViste)];
+            idViste = [...new Set(idViste)];
 
-              return reportService.getDatiSchedaDiRiferimento(idScheda);
-            }).then(function (res) {
-              infoScheda = res;
+            return reportService.getDatiSchedaDiRiferimento(idScheda);
+          }).then(function (res) {
+            infoScheda = res;
 
             const promises = [];
             if (idScheda && arrayIdRecords.length > 0) {
               arrayIdRecords.forEach((idR, index) => {
-                 // TO-DO: MODIFICARE QUESTA RIGA PER LAVORARE SULLA CREAZIONE DI REPORT CONTENENTI DATI DI VISTE MULTIPLE
+                // TO-DO: MODIFICARE QUESTA RIGA PER LAVORARE SULLA CREAZIONE DI REPORT CONTENENTI DATI DI UNA STESSA SCHEDA 
+                // MA MULTIPLI ID RECORDS
                 if (index > 0) return;
 
                 promises.push(xdbApiService.getValoriCampiScheda(idScheda, idR));
@@ -152,43 +154,43 @@
               }
             });
 
-              const vistaRowsPromisesList = vistaRowsParamsList.reduce((promisesArray, vistaRowsParams) => {
-                const resultArr = [...promisesArray];
-                const foreignKeyVista = vistaRowsParams?.foreignKeyVista;
+            const vistaRowsPromisesList = vistaRowsParamsList.reduce((promisesArray, vistaRowsParams) => {
+              const resultArr = [...promisesArray];
+              const foreignKeyVista = vistaRowsParams?.foreignKeyVista;
 
-                if (arrayIdRecords.length > 0) {
-                  const multipleIdsQuery = handleIdRecordsParams.makeVistaRowsQueryParams(foreignKeyVista, arrayIdRecords);
-                  
-                  multipleIdsQuery.forEach((idQ, index) => {
-                     // TO-DO: MODIFICARE QUESTA RIGA PER LAVORARE SULLA CREAZIONE DI REPORT CONTENENTI DATI DI VISTE MULTIPLE
-                if (index > 0) return;
+              if (arrayIdRecords.length > 0) {
+                const multipleIdsQuery = handleIdRecordsParams.makeVistaRowsQueryParams(foreignKeyVista, arrayIdRecords);
 
-                    resultArr.push(xdbApiService.getVistaRows(
-                      vistaRowsParams.idVista,
-                      vistaRowsParams.limit,
-                      vistaRowsParams.offset,
-                      vistaRowsParams.sort,
-                      idQ
-                    ));
-                  });
+                multipleIdsQuery.forEach((idQ, index) => {
+                  // TO-DO: MODIFICARE QUESTA RIGA PER LAVORARE SULLA CREAZIONE DI REPORT CONTENENTI DATI DI VISTE MULTIPLE
+                  if (index > 0) return;
 
-                  return resultArr;
-                }
-
-                const singleIdQuery = handleIdRecordsParams.makeVistaRowsQueryParams(foreignKeyVista, intIdRecord);
-                resultArr.push(xdbApiService.getVistaRows(
-                  vistaRowsParams.idVista,
-                  vistaRowsParams.limit,
-                  vistaRowsParams.offset,
-                  vistaRowsParams.sort,
-                  singleIdQuery
-                ));
+                  resultArr.push(xdbApiService.getVistaRows(
+                    vistaRowsParams.idVista,
+                    vistaRowsParams.limit,
+                    vistaRowsParams.offset,
+                    vistaRowsParams.sort,
+                    idQ
+                  ));
+                });
 
                 return resultArr;
+              }
 
-              }, []) ?? [];
-              
-              promises.push(...vistaRowsPromisesList);
+              const singleIdQuery = handleIdRecordsParams.makeVistaRowsQueryParams(foreignKeyVista, intIdRecord);
+              resultArr.push(xdbApiService.getVistaRows(
+                vistaRowsParams.idVista,
+                vistaRowsParams.limit,
+                vistaRowsParams.offset,
+                vistaRowsParams.sort,
+                singleIdQuery
+              ));
+
+              return resultArr;
+
+            }, []) ?? [];
+
+            promises.push(...vistaRowsPromisesList);
             if (promises.length) {
               return Promise.all(promises);
             }
@@ -208,116 +210,116 @@
                   etichettaVista: vistaCorrelata.etichettaVista
                 };
 
-                  Object.assign($scope, reportHelpers.mapVistaToReportData(infoVista, vista.data));
-                });
-              }
-
-              const body = document.querySelector('.report-wrapper');
-              const elements = $compile(template)($scope);
-
-              body.innerHTML = '';
-
-              Array.from(elements).forEach(element => {
-                body.appendChild(element);
+                Object.assign($scope, reportHelpers.mapVistaToReportData(infoVista, vista.data));
               });
+            }
 
-              const valoriCampiEditabili = storageService.loadLocalStorage('__valoriCampiEditabili') || {};
+            const body = document.querySelector('.report-wrapper');
+            const elements = $compile(template)($scope);
 
-              const div = document.createElement('div');
-              div.innerHTML = JSON.stringify(valoriCampiEditabili);
-              body.appendChild(div);
+            body.innerHTML = '';
 
-              domUtilsService.waitForSelector('[data-attach-logo-aziendale]').then((reportCompanyLogo) => {
-                const ID_RECORD = 1;
-                avatars.get(ID_SCHEDA_CONFIGURAZIONE, ID_RECORD)
-                  .then((url) => {
-                    reportCompanyLogo.src = `${url}`;
+            Array.from(elements).forEach(element => {
+              body.appendChild(element);
+            });
+
+            const valoriCampiEditabili = storageService.loadLocalStorage('__valoriCampiEditabili') || {};
+
+            const div = document.createElement('div');
+            div.innerHTML = JSON.stringify(valoriCampiEditabili);
+            body.appendChild(div);
+
+            domUtilsService.waitForSelector('[data-attach-logo-aziendale]').then((reportCompanyLogo) => {
+              const ID_RECORD = 1;
+              avatars.get(ID_SCHEDA_CONFIGURAZIONE, ID_RECORD)
+                .then((url) => {
+                  reportCompanyLogo.src = `${url}`;
+                });
+            });
+
+            domUtilsService.waitForSelector('[data-prima-foto-report]').then((primaFoto) => {
+              const idVista = primaFoto.dataset.primaFotoReport;
+              const nomeRisorsa = primaFoto.dataset.risorsa;
+              const idCampo = primaFoto.dataset.campo;
+              const filtroPerCampo = primaFoto.dataset.filtro;
+
+              // genere la query per andare a recuperare i dati per una persona specifica
+              const q = filtroPerCampo ? `${filtroPerCampo}${handleIdRecordsParams.validateIdRecordParam(intIdRecord)}` : null;
+              xdbApiService.getVistaRows(idVista, 1, 0, null, q).then((res) => {
+                const records = res.data.records ?? [];
+                const image = records.filter(file => filesPerCampo.isImage(file.nome));
+                filesPerCampo
+                  .download(
+                    nomeRisorsa,
+                    image[0].ID,
+                    idCampo
+                  ).then((url) => {
+                    primaFoto.src = `${url}`;
                   });
-              });
-
-              domUtilsService.waitForSelector('[data-prima-foto-report]').then((primaFoto) => {
-                const idVista = primaFoto.dataset.primaFotoReport;
-                const nomeRisorsa = primaFoto.dataset.risorsa;
-                const idCampo = primaFoto.dataset.campo;
-                const filtroPerCampo = primaFoto.dataset.filtro;
-
-                // genere la query per andare a recuperare i dati per una persona specifica
-                const q = filtroPerCampo ? `${filtroPerCampo}${handleIdRecordsParams.validateIdRecordParam(intIdRecord)}` : null;
-                xdbApiService.getVistaRows(idVista, 1, 0, null, q).then((res) => {
-                  const records = res.data.records ?? [];
-                  const image = records.filter(file => filesPerCampo.isImage(file.nome));
-                  filesPerCampo
-                    .download(
-                      nomeRisorsa,
-                      image[0].ID,
-                      idCampo
-                    ).then((url) => {
-                      primaFoto.src = `${url}`;
-                    });
-                });
-              });
-
-              reportService.getApiTemplateCss(parseInt(searchParams.get('idTemplate'), 10)).then((res) => {
-                const withPrintInstructions = res.length > 0 ? `@media print {
-             ${res} 
-            }` : '';
-
-                const styleTags = document.querySelectorAll("style");
-                const styleTag = styleTags[styleTags.length - 1];
-
-                if (!styleTag) {
-                  const newStyleTag = document.createElement('style');
-                  newStyleTag.textContent = res;
-                  document.head.appendChild(newStyleTag)
-                } else {
-                  styleTag.textContent = `${styleTag.textContent}\n${res}\n${withPrintInstructions}`;
-                }
-
-              })       
-
-              return campiEditabiliReport.applyValues(valoriCampiEditabili).then(() => {
-                const images = body.querySelectorAll('[data-avatar-record]');
-
-                images.forEach(image => {
-                  const idSschedaPerAvatar = image.dataset.avatarRecord || idScheda;
-                  avatars
-                    .get(idSschedaPerAvatar, intIdRecord)
-                    .then(url => {
-                      const div = document.createElement('div');
-                      div.style.width = `${image.width}px`;
-                      div.style.height = `${image.height}px`;
-                      div.style.backgroundImage = `url('${url}')`;
-                      div.style.backgroundPosition = 'center';
-                      div.style.backgroundSize = 'cover';
-
-                      image.replaceWith(div);
-                    });
-                });
-              });
-
-            }).catch(function (e) {
-              printError(e);
-            }).finally(function () {
-              $scope.$applyAsync(function () {
-                $scope.loading = false;
-                const reportHeader = document.getElementById('header');
-                if (reportHeader) {
-                  setTimeout(function () {
-                    const headerHeight = reportHeader.offsetHeight;
-                    const reportHeight = document.documentElement.clientHeight;
-                    if (headerHeight / reportHeight > 0.15) {
-                      window.top.postMessage('showReportHeaderWarning', '*');
-                    } else {
-                      window.top.postMessage('hideReportHeaderWarning', '*');
-                    }
-                  }, 0);
-                }
               });
             });
 
-          } catch (error) {
-            printError(error);
-          }
+            reportService.getApiTemplateCss(parseInt(searchParams.get('idTemplate'), 10)).then((res) => {
+              const withPrintInstructions = res.length > 0 ? `@media print {
+             ${res} 
+            }` : '';
+
+              const styleTags = document.querySelectorAll("style");
+              const styleTag = styleTags[styleTags.length - 1];
+
+              if (!styleTag) {
+                const newStyleTag = document.createElement('style');
+                newStyleTag.textContent = res;
+                document.head.appendChild(newStyleTag)
+              } else {
+                styleTag.textContent = `${styleTag.textContent}\n${res}\n${withPrintInstructions}`;
+              }
+
+            })
+
+            return campiEditabiliReport.applyValues(valoriCampiEditabili).then(() => {
+              const images = body.querySelectorAll('[data-avatar-record]');
+
+              images.forEach(image => {
+                const idSschedaPerAvatar = image.dataset.avatarRecord || idScheda;
+                avatars
+                  .get(idSschedaPerAvatar, intIdRecord)
+                  .then(url => {
+                    const div = document.createElement('div');
+                    div.style.width = `${image.width}px`;
+                    div.style.height = `${image.height}px`;
+                    div.style.backgroundImage = `url('${url}')`;
+                    div.style.backgroundPosition = 'center';
+                    div.style.backgroundSize = 'cover';
+
+                    image.replaceWith(div);
+                  });
+              });
+            });
+
+          }).catch(function (e) {
+            printError(e);
+          }).finally(function () {
+            $scope.$applyAsync(function () {
+              $scope.loading = false;
+              const reportHeader = document.getElementById('header');
+              if (reportHeader) {
+                setTimeout(function () {
+                  const headerHeight = reportHeader.offsetHeight;
+                  const reportHeight = document.documentElement.clientHeight;
+                  if (headerHeight / reportHeight > 0.15) {
+                    window.top.postMessage('showReportHeaderWarning', '*');
+                  } else {
+                    window.top.postMessage('hideReportHeaderWarning', '*');
+                  }
+                }, 0);
+              }
+            });
+          });
+
+        } catch (error) {
+          printError(error);
         }
+      }
     ]);
 })();
