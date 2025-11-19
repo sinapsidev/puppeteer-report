b0acbcb50a6a49407e4d27bfeac691411a5c21c3
valentinamarrazzo <valemarr99@gmail.com>
2025-11-19 15:00:29 +0100

    feat(vistaDataStore): aggiunto service con funzione di store per dati vista

---
+++ server/public/modules/report/stores/vistaDataStore.js
@@ -0,0 +1,35 @@
+// nonostante sia vero che il concetto di store non esisteva ancora per le versioni
+// 1.x di Angular, lo ripropongo ugualmente attraverso l'uso di questo service 
+// come tale
+
+'use strict';
+(function () {
+    window.angular.module('reportApp.report').service('vistaDataStore', function () {
+        this.data = {
+            idRecord: -1,
+            idRecords: [],
+            visteCorrelate: [],
+            idViste: [],
+        };
+
+        this.setData = function (newObj = {
+            idRecord: -1,
+            idRecords: [],
+            visteCorrelate: [],
+            idViste: [],
+        }) {
+            const canUpdateObject = () => {
+                const newObjKeys = Object.keys(newObj);
+                const oldObjKeys = Object.keys(this.data);
+
+                return newObjKeys.every((key) => !!oldObjKeys.find(key));
+            };
+
+            if (!canUpdateObject()) throw new Error(`Non è possibile aggiornare l\'oggetto in vistaDataStore con {${Object.keys(newObj)}}`);
+
+            const dataUpdated = { ...this.data, ...newObj };
+
+            this.data = dataUpdated;
+         };
+    });
+})();
