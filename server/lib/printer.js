e3337642f46958f5a11338c9ca7989ca13956b6e
valentinamarrazzo <valemarr99@gmail.com>
2025-12-10 10:38:33 +0100

    fix(printer): sostituito uso di config.footerTemplate con FOOTER_TEMPLATE

--- server/lib/printer.js
+++ server/lib/printer.js
@@ -368,7 +368,7 @@ const create = async ({ timeout, logger, networkLogging, cluster }) => {
         ${CUSTOM_CSS}
       </style>
       <div class="page-footer page-footer--with-numbers">
-        ${!config?.footerTemplate ? "" : config?.footerTemplate }
+        ${!HAS_FOOTER ? "" : FOOTER_TEMPLATE}
         <div id="page-numbers-container" class="page-footer">
           Pagina <span class="pageNumber"></span> di <span class="totalPages"></span>
         </div>
