1d3b885a9d0879ba4e484cc891e6458fd83a5eb7
valentinamarrazzo <valemarr99@gmail.com>
2025-12-10 11:25:32 +0100

    fix(printer): corretto nome proprietà body per flag page number

--- server/lib/printer.js
+++ server/lib/printer.js
@@ -101,7 +101,7 @@ const create = async ({ timeout, logger, networkLogging, cluster }) => {
 
     const WIDTH = body.width + 'mm';
     const HEIGHT = body.height + 'mm';
-    const IS_PAGE_NUMBER_VISIBLE = body.pageNumber;
+    const IS_PAGE_NUMBER_VISIBLE = body.insertPageNumber;
 
     await page.waitForSelector('#header', { timeout: 0, visible: true });
     await page.waitForSelector('#footer', { timeout: 0, visible: true });
