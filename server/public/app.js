dd79d915bb61271ea4d06897daf5ecd95a147369
valentinamarrazzo <valemarr99@gmail.com>
2025-12-18 12:43:58 +0100

    fix(app): separata gestione avatars record dalla promise per campi editabili

--- server/public/app.js
+++ server/public/app.js
@@ -184,6 +184,28 @@
                 });
             });
 
+            domUtilsService.waitForSelector('[data-avatar-record]').then((reportCompanyLogo) => {
+              if (!reportCompanyLogo) return;
+                
+               const images = body.querySelectorAll('[data-avatar-record]');
+
+              images.forEach(image => {
+                const idSschedaPerAvatar = image.dataset.avatarRecord || idScheda;
+                avatars
+                  .get(idSschedaPerAvatar, intIdRecord)
+                  .then(url => {
+                    const div = document.createElement('div');
+                    div.style.width = `${image.width}px`;
+                    div.style.height = `${image.height}px`;
+                    div.style.backgroundImage = `url('${url}')`;
+                    div.style.backgroundPosition = 'center';
+                    div.style.backgroundSize = 'cover';
+
+                    image.replaceWith(div);
+                  });
+              });
+            });
+
             domUtilsService.waitForSelector('[data-prima-foto-report]').then((primaFoto) => {
               const idVista = primaFoto.dataset.primaFotoReport;
               const nomeRisorsa = primaFoto.dataset.risorsa;
@@ -224,26 +246,8 @@
 
             })
 
-            return campiEditabiliReport.applyValues(valoriCampiEditabili).then(() => {
-              const images = body.querySelectorAll('[data-avatar-record]');
-
-              images.forEach(image => {
-                const idSschedaPerAvatar = image.dataset.avatarRecord || idScheda;
-                avatars
-                  .get(idSschedaPerAvatar, intIdRecord)
-                  .then(url => {
-                    const div = document.createElement('div');
-                    div.style.width = `${image.width}px`;
-                    div.style.height = `${image.height}px`;
-                    div.style.backgroundImage = `url('${url}')`;
-                    div.style.backgroundPosition = 'center';
-                    div.style.backgroundSize = 'cover';
-
-                    image.replaceWith(div);
-                  });
-              });
-            });
-
+            return campiEditabiliReport.applyValues(valoriCampiEditabili)
+            
           }).catch(function (e) {
             printError(e);
           }).finally(function () {
