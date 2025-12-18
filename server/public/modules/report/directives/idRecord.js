d3c42ea77b401f0f3d9f0480dae11005523b5017
valentinamarrazzo <valemarr99@gmail.com>
2025-12-18 12:46:14 +0100

    fix(idRecord): il lancio delle operazioni avviene quando termina loading
    quindi eliminato timeout

--- server/public/modules/report/directives/idRecord.js
+++ server/public/modules/report/directives/idRecord.js
@@ -1,59 +1,66 @@
 'use strict';
 (function () {
-    window.angular.module('reportApp.report').directive('idRecord', function ($timeout, $compile, visteDataService, vistaDataStore) {
+    window.angular.module('reportApp.report').directive('idRecord', function ($compile, visteDataService, vistaDataStore) {
         return {
             restrict: 'A',
             scope: true,
             transclude: true,
             link: {
                 post(scope, element, attrs, _controller, transclude) {
-                    let transcludeFnScope;
+                    let transcludeFnScope = null;
 
                     const idRecord = parseInt(attrs.idRecord, 10);
 
-                    $timeout(function () {
-                        const { visteCorrelate, idViste } = vistaDataStore.getData();
+                    scope.$watch(() => scope.$parent.loading, function (newValue, oldValue) {
+                        if (!newValue && oldValue) {
 
-                        const vistaRowsPromisesList = visteDataService.getVistaRowsPromisesList(idViste, idRecord, visteCorrelate);
-
-                        transcludeFnScope = scope.$parent.$new();
-
-                        const compileNewScopeContent = (promisesList) => {
-                            return Promise.all(promisesList)
-                                .then((res) => {
-                                    if (!res?.length) return;
-
-                                    return res
-                                        .filter((vista) => vista?.data)
-                                        .map(function (vista) {
-                                            const vistaCorrelata = visteCorrelate.find(function (v) { return v.idVista === vista?.data?.id; }) || {};
-
-                                            return visteDataService.createReportVistaObject({ vistaCorrelata, vistaResult: vista.data });
-                                        }) ?? [];
-                                })
-                                .then((reportData) => {
-                                    reportData.forEach(function (vistaScopeObj) {
-                                        Object.assign(transcludeFnScope, vistaScopeObj);
+                            const { visteCorrelate, idViste } = vistaDataStore.getData();
+    
+                            const vistaRowsPromisesList = visteDataService.getVistaRowsPromisesList(idViste, idRecord, visteCorrelate);
+    
+                            transcludeFnScope = scope.$parent.$new();
+    
+                            const compileNewScopeContent = (promisesList) => {
+                                return Promise.all(promisesList)
+                                    .then((res) => {
+                                        if (!res?.length) return;
+    
+                                        return res
+                                            .filter((vista) => vista?.data)
+                                            .map(function (vista) {
+                                                const vistaCorrelata = visteCorrelate.find(function (v) { return v.idVista === vista?.data?.id; }) || {};
+    
+                                                return visteDataService.createReportVistaObject({ vistaCorrelata, vistaResult: vista.data });
+                                            }) ?? [];
                                     })
+                                    .then((reportData) => {
+                                        reportData.forEach(function (vistaScopeObj) {
+                                            Object.assign(transcludeFnScope, vistaScopeObj);
+                                        })
+    
+                                        const parentElement = element.parent();
+    
+                                        transclude(transcludeFnScope, function (clone) {
+                                            const elementClone = angular.element(clone);
+                                            element.append($compile(elementClone)(transcludeFnScope));
+                                        }, parentElement);
+    
+                                    })
+                                    .finally(() => {
+                                        if (!scope.$$phase) {
+                                            scope.$parent.$digest();
+                                        }
+                                    });
+                            };
+    
+                            vistaRowsPromisesList?.length > 0 && compileNewScopeContent(vistaRowsPromisesList);
+                        }
 
-                                    const parentElement = element.parent();
+                        if (newValue && transcludeFnScope) {
+                            transcludeFnScope.$destroy();
+                        }
 
-                                    transclude(transcludeFnScope, function (clone) {
-                                        const elementClone = angular.element(clone);
-                                        element.append($compile(elementClone)(transcludeFnScope));
-                                    }, parentElement);
-
-                                })
-                                .finally(() => {
-                                    if (!scope.$$phase) {
-                                        scope.$parent.$digest();
-                                    }
-                                });
-                        };
-
-                        vistaRowsPromisesList?.length > 0 && compileNewScopeContent(vistaRowsPromisesList);
-
-                    }, 100)
+                    })
                 }
             },
         }
